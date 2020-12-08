3fde2a670120a38f9f59de6f17aba826e72fea5c
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-08 18:42:12 -0500

    adds load string utility

--- src/util/index.js
+++ src/util/index.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const loadInput = (dirPath) => {
+
+const loadInputString = (dirPath) => {
   const result = fs.readFileSync(path.join(dirPath, '/input.txt'), 'utf8');
+
+  if (result.charCodeAt(result.length - 1) === 10) {
+    return result.slice(0, result.length - 1);
+  } else {
+    return result;
+  }
+}
+
+const loadInput = (dirPath) => {
+  const result = loadInputString(dirPath);
   return result.split('\n').filter(a => a.length > 0);
 };
 
-module.exports = { loadInput };
+
+module.exports = { loadInput, loadInputString };
