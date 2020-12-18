86ca25ae5e6f6d9b3e8ae38517b6403997c56188
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-18 12:51:03 -0500

    adds more utility functions

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const loadInputString = (dirPath) => {
   const result = fs.readFileSync(path.join(dirPath, '/input.txt'), 'utf8');
 
@@ -17,5 +16,53 @@ const loadInput = (dirPath, separator = '\n') => {
   return result.split(separator).filter(a => a.length > 0);
 };
 
+const print2DMatrix = (matrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      process.stdout.write(matrix[i][j]);
+    }
 
-module.exports = { loadInput, loadInputString };
+    process.stdout.write('\n');
+  }
+};
+
+const print3DMatrix = (matrix, offset = 0) => {
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(`----- z = ${i + offset}`);
+    print2DMatrix(matrix[i]);
+  }
+};
+
+const initialize2DMatrix = (rows, cols, initialValue = 0) => {
+  const matrix;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i]) {
+        matrix[i][j] = initialValue;
+      } else {
+        matrix[i] = [initialValue];
+      }
+    }
+  }
+
+  return matrix;
+};
+
+const initialize3DMatrix = (depth, rows, cols, initialValue = 0) => {
+  const matrix = [];
+  for (let i = 0; i < depth; i++) {
+    matrix[i] = initialize2DMatrix(rows, cols, initialValue);
+  }
+
+  return matrix;
+};
+
+module.exports = {
+  loadInput,
+  loadInputString,
+  print2DMatrix,
+  print3DMatrix,
+  initialize2DMatrix,
+  initialize3DMatrix,
+};
